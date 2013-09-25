79380b21d5deb37971ff4c556b24c5e17b695489
Lakhveer Jajj <lakhveer@gmail.com>
2013-09-25 13:52:08 -0400

    Added skeleton code
    Added FileParser class and Indexer

--- vertorir.py
+++ vertorir.py
@@ -1,5 +1,23 @@
 import sys
 import os
+import getopt
+
+class FileParser:
+	def __init__(self):
+		#set file path and open file
+		pass
+	def parseFile(self):
+		#Reads file line by line and calls other methods for each line event
+		pass
+	def commonWords(self):
+		#Read common word file and load it into a array
+		pass
+	
+
+class Indexer:
+	def __init__(self):
+		pass
+
 
 def main(argv):
     print 'Hello World'
