ab2acae14e327d6b065bc0b84a4200f8283323e5
Lakhveer Jajj <lakhveer@gmail.com>
2013-09-26 13:37:42 -0400

    Added Alvin's changes
    via email

--- vectorir.py
+++ vectorir.py
@@ -5,19 +5,46 @@ import getopt
 class FileParser:
 	def __init__(self, path):
 		self.path = path
+		self.wordList = []
+		self.cWords = []
+		self.flag = 0
 	def parseFile(self):
 		#Reads file line by line and calls other methods for each line event
+		file = open(self.path, 'r')
+		for line in file:
+			for word in line.split():
+				if word == '.I':
+					pass
+				if word == '.T':
+					pass
+				if word == '.W':
+					pass			
 		pass
 	def commonWords(self):
 		#Read common word file and load it into a array
+		file = open('CACM/common_words', 'r')
+		for line in file:
+			for word in line.split():
+				self.cWords.append(word)
 		pass
 	
+class Tree:
+	#Indexer data structure
+	def __init__(self):
+		pass
+
+class Node:
+	#Elements in a tree
+	def __init__(self):
+
 
 class Indexer:
 	def __init__(self):
+		self.terms = [['google', [(1,1),(1,50)]],['cat',[(2,3)]]] #practic terms
 		pass
 	def buildTree(self):
 		#Build tree
+		print self.terms[0][1][1]
 		pass
 
 
@@ -40,6 +67,13 @@ def main(argv):
 			sys.exit()
 		elif opt in ("-i", "--ifile"):
 			inputfile = arg
+	test = Indexer()
+	test.buildTree()
+
+	
+    x = FileParser(inputfile)
+	x.parseFile()
+	x.commonWords()
 
     #print('Hello')
 if __name__ == '__main__':
